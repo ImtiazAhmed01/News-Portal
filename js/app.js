97451b6356c66858c0f258254b934534c830e312
ImtiazAhmed01 <imtiazahm21@gmail.com>
2022-09-03 19:35:14 +0600

    more code added

--- js/app.js
+++ js/app.js
@@ -1,31 +1,25 @@
-const loadNews = async (clickCategory) => {
-    const res = await fetch('https://openapi.programming-hero.com/api/news/${clickCategory}');
-    const data = await res.json();
-    const mainData = data.data.news_category;
-    // displayCatagories(data.data.news_category);
-    return mainData;
+const loadAllNews = async () => {
+    const response = await fetch("https://openapi.programming-hero.com/api/news/categories");
+    const data = await response.json();
+    //const allData = data.data.news_category;
+    return (data.data.news_category);
+
 }
 
-//Display catagory
-const displayCatagories = async () => {
-    const news = await loadNews();
-    const allCategory = document.getElementById('all-category');
-    for (const category of news) {
-        const li = document.createElement('li');
-        li.innerHTML = `<li id="click-category" class="list-group-item list-group-item-primary rounded ms-5 p-3" onclick ="loadData('0${category_id}')>${category.category_name}</li>`;
-
-        allCategory.appendChild(li);
-
+const loadAllCategories = async () => {
+    const data = await loadAllNews();
+    const allCategories = document.getElementById('all-category');
+    for (const categories of data) {
+        //console.log(categories.category_name);
+        const li = document.createElement("li");
+        li.innerHTML = `
+        <li class="nav-item px-5">
+            <a class="nav-link active text-light rounded p-2" onclikc="">${categories.category_name}</a>
+        </li>
+        `;
+        allCategories.appendChild(li);
     }
 }
 
-// const loadData = news=>{
-//     const clickCategory = document.getElementById('clicked-category');
-// }
-
-// .addEventListener('click', function () {
-//     loadNews(clickCategory);
-// })
-
-displayCatagories();
-//loadNews();
+loadAllCategories();
+//loadAllNews();
